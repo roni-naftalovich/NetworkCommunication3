3c3e869e08f314f77de9abacad1669c52730dd5c
roni-naftalovich <157619305+roni-naftalovich@users.noreply.github.com>
2024-03-06 20:39:32 +0200

    Add files via upload

---
+++ RUDP_Receiver.c
@@ -0,0 +1,159 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <arpa/inet.h> 
+#include <sys/socket.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <time.h> // For the time function
+#include <sys/time.h> // For the timeval structure
+#include <netinet/tcp.h> 
+#include "RUDP_API.h" 
+
+
+#define DEFAULT_IP "127.0.0.1"
+#define DEFAULT_PORT 9998
+#define MAX_CLIENTS 1
+#define BUFFER_SIZE 1024
+#define MAX_RUNS 50
+#define SIZE_OF_FILE 2800000
+#define FILENAME "stats.txt"
+
+
+int main(int args, char** argv) {
+    printf("Starting RUDP Receiver...\n");
+    int port = DEFAULT_PORT;
+    if (args != 3) {
+        printf("Invalid command, system use default port\n");
+    } else if (args ==3) {
+        port = atoi(argv[2]);
+
+    }
+
+    int sock;
+    struct timeval start, end;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    // Reset the server and client structures to zeros.
+    memset(&server_addr, 0, sizeof(server_addr));
+    memset(&client_addr, 0, client_len);
+    
+
+    // The variable to store the socket option for reusing the server's address.
+    int opt = 1;
+
+    sock = rudp_socket(opt);
+    if (sock <0 ) {
+        perror("rudp_socket_setup(2)");
+        return 1;
+    }
+
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+
+     // Try to bind the socket to the server's address.
+    if (bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        perror("bind(2)");
+        close(sock);
+        return -1;
+        }
+
+    fprintf(stdout, "Listening for incoming connections ...\n");
+        
+
+
+    int client_sock = rudp_accept(sock, (struct sockaddr_in *)&client_addr, &client_len);
+    if (client_sock <0){
+        perror("rudp_accept(2)");
+        close(sock);
+        return 1;
+    }
+    fprintf(stdout, "Handshake succesfuly done, accepted connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+    int total_bytes_received = 0;
+    int bytes_received=0;
+    int counter = 0;
+    int packages_received = 0;
+    double speed_sum = 0.0;
+    double time_sum = 0.0;
+    char buffer[BUFFER_SIZE];
+
+    FILE *stats_file = fopen(FILENAME, "w");
+    if (stats_file == NULL) {
+        perror("fopen");
+        return 1;
+    }
+
+    while(1){
+        bytes_received=0;
+        counter = 0;
+        total_bytes_received = 0;
+        memset(buffer, 0, BUFFER_SIZE);
+
+
+        while(total_bytes_received<SIZE_OF_FILE){
+            bytes_received=0;
+            bytes_received = rudp_recvfrom(sock,(struct sockaddr_in *)&client_addr, &client_len);
+            if (bytes_received ==- 1) {
+                perror("rudp_recvfrom(2)");
+                close(sock);
+                return 1;
+            }
+            if(counter == 0){
+                gettimeofday(&start, NULL);
+            }
+            if (bytes_received == -2) {
+                close(sock);
+                break;
+            }
+            total_bytes_received += bytes_received;
+            counter++;
+            memset(buffer, 0, sizeof(buffer));
+
+
+        }
+
+        gettimeofday(&end, NULL);
+        if(bytes_received==-2){
+            break;
+        }
+
+
+        fprintf(stdout, "File transfer complete\n");
+        double this_elapsed_time = ((end.tv_sec - start.tv_sec) * 1000.0) + ((end.tv_usec - start.tv_usec) / 1000.0);
+        double this_speed = (total_bytes_received / 1024.0 / 1024.0) / (this_elapsed_time / 1000.0);
+        speed_sum = speed_sum + this_speed;
+        time_sum = time_sum + this_elapsed_time;
+
+        packages_received++;
+
+        fprintf(stats_file, "- Run #%d Data: Time = %.2f ms; Speed = %.2f MB/s\n", packages_received, this_elapsed_time, this_speed);
+
+        // Print the sent message.
+        printf( "waiting for the Senders response...\n");
+    }
+    fclose(stats_file);
+
+    close(sock);
+    printf("closed the client socket\n");
+    // ... (Print statistics and times)
+    printf("----------------------------------\n");
+    printf("-           * Statistics *       -\n");
+    stats_file = fopen(FILENAME, "r");
+    if (stats_file == NULL) {
+        perror("fopen");
+        return 1;
+    }
+
+    while (fgets(buffer, sizeof(buffer), stats_file) != NULL) {
+        printf("%s", buffer);
+    }
+    printf("-\n");
+    printf("- Average time: %.2fms\n", time_sum / packages_received);
+    printf("- Average bandwidth: %.2fMB/s\n", speed_sum / packages_received);
+    printf("----------------------------------\n");
+    printf( "Receiver end.\n");
+
+return 0;
+}
--- RUDP_Sender.c
+++ RUDP_Sender.c
@@ -1,109 +1,131 @@
-#include <stdio.h>
-#include <time.h>
-#include <string.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <arpa/inet.h>
-#include "stdlib.h"
-#include <sys/socket.h>
-#include <unistd.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <arpa/inet.h> 
+#include <sys/socket.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <time.h> // For the time function
+#include <sys/time.h> // For the timeval structure
+#include <netinet/tcp.h> 
 #include "RUDP_API.h"
-
+#define BUFFER_SIZE 1024
+#define TIMEOUT_SECONDS 2
+#define DEFAULT_IP "127.0.0.1"
 #define DEFAULT_PORT 9998
-#define RECEIVER_IP "127.0.0.1"
-#define BUFFER_SIZE 2000000
-#define SIZE_OF_FILE 2000000
-#define EXIT_MESSAGE "EXIT"
+#define SIZE_OF_FILE 2800000
 
-/*
-* @brief A random data generator function based on srand() and rand().
-* @param size The size of the data to generate (up to 2^32 bytes).
-* @return A pointer to the buffer.
-*/
+
+
 char *util_generate_random_data(unsigned int size) {
     char *buffer = NULL;
-    // Argument check.
-    if (size == 0)
+// Argument check.
+    if (size == 0) {
         return NULL;
+    }
     buffer = (char *) calloc(size, sizeof(char));
-    // Error checking.
-    if (buffer == NULL)
+    if (buffer == NULL){
+        printf("calloc(3) failed\n");
         return NULL;
-    // Randomize the seed of the random number generator.
+    }
+// Randomize the seed of the random number generator.
     srand(time(NULL));
-    for (unsigned int i = 0; i < size; i++)
+    for (unsigned int i = 0; i < size; i++) {
         *(buffer + i) = ((unsigned int) rand() % 256);
+    }
     return buffer;
 }
 
-int main(int argc, char *argv[]) {
 
-    int port= SENDER_PORT
-    printf("Starting Sender...\n");
+int main(int args, char** argv){
+    printf("RUDP Sender starting\n");
+    char *server_ip = DEFAULT_IP;
+    int port = DEFAULT_PORT;
 
-    if (argc != 5) {
-        puts("invalid command");
+    if (args != 5) {
+        printf("Invalid command, system use default port and ip\n");
+    } else if (args == 5) {
+        server_ip = argv[2];
+        port = atoi(argv[4]);
+    }
+
+    int sock = -1;
+    int opt = 1;
+    struct sockaddr_in server;
+    char *data = util_generate_random_data(SIZE_OF_FILE);
+
+    memset(&server, 0, sizeof(server));
+    sock = rudp_socket(opt);
+    if (sock <0){
+        perror("rudp_socket(2)");
         return 1;
     }
-    char SERVER_IP[20] = {0};
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-p") == 0) {
-            port = atoi(argv[i + 1]);
-        } else if (strcmp(argv[i], "-ip") == 0) {
-            strcpy(SERVER_IP, argv[i + 1]);
-        }
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port); // Todo: set the server's port
+
+     if (inet_pton(AF_INET, server_ip , &server.sin_addr) <= 0) { // setting the SERVER_IP address
+        perror("inet_pton(3)");
+        close(sock);
+        return 1;
     }
 
-    // Create socket
-    int sockfd = rudp_socket();
-
-    // Set up receiver address
-    struct sockaddr_in receiver_addr;
-    //socklen_t rec_len = sizeof(receiver_addr);
-    memset(&receiver_addr, 0, sizeof(receiver_addr));
-    receiver_addr.sin_family = AF_INET;
-    receiver_addr.sin_port = htons(port);
-
-    if (inet_pton(AF_INET, SERVER_IP, &receiver_addr.sin_addr) <= 0) { // setting the SERVER_IP address
-        perror("inet_pton(3)");
-        close(sockfd);
+    fprintf(stdout, "Waiting for RUDP Connection...\n");
+    if(rudp_connect(sock, (struct sockaddr_in *)&server, sizeof(server)) < 0){
+//        perror("rudp_connect(2)");
+        rudp_close(sock);
         return -1;
     }
-
-    char *file = util_generate_random_data(FILE_SIZE);
-    int sending = 1;
-
-    while (sending == 1) {
-        int handShake = rudp_connect(sockfd, &receiver_addr, sizeof(receiver_addr), 0);
-        if (handShake != 0) {
-            perror("connection failed");
-            return -1;
-        }
-        printf("Handshke performed successfully\n");
-
-        // Send data
-        if (rudp_send_file(file, sockfd, receiver_addr, handShake) < 0) {
-            perror("send file failed");
-            return -1;
-        }
-        
-
-        printf("data sent.\n");
-        //finish sending, and receive the last ack, close the rudp connection
-        //packet->flags = FLAG_FIN;
-        if (rudp_close(sockfd, &receiver_addr, sizeof(receiver_addr), 0) == -1) {
-            perror("close failed");
-
-            return -1;
-        }
-
-        printf("connection closed\n");
-
-
-        printf("Press 1 to send file again or 0 to exit:\n");
-        scanf("%d", &sending);
+    fprintf(stdout, "Handshake succesfuly done, connected to the server!\n"
+                    "Sending data to the server\n");
+//    if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, CC_algo, strlen(CC_algo)) < 0) {
+//        perror("setsockopt(2)");
+//        close(sock);
+//        return 1;
+//    }
+    int bytes_sent = rudp_sendto(sock, data, SIZE_OF_FILE, (struct sockaddr_in *)&server, sizeof(server));
+    if (bytes_sent < 0) {
+        perror("rudp_sendto(2)");
+        rudp_close(sock);
+        return -1;
     }
-    close(sockfd);
-    //to print stats
+    fprintf(stdout, "data sent successfully!\n");
+    while (1){
+        printf("Press 1 to resend the data or 0 to exit\n");
+        int choice;
+        if(scanf("%d", &choice)<0){
+            perror("scanf(2)");
+            close(sock);
+            break;
+        }
+        if(choice == 0){
+            if(rudp_dissconnect_client(sock, (struct sockaddr_in *)&server, sizeof(server)) < 0){
+                perror("rudp_dissconnect_client(2)");
+                rudp_close(sock);
+                return -1;
+            }
+            printf("Disconnected from the server\n");
+            rudp_close(sock);
+            break;
+        }
+        if(choice == 1){
+            bytes_sent= rudp_sendto(sock, data, SIZE_OF_FILE, (struct sockaddr_in *)&server, sizeof(server));
+            if (bytes_sent < 0) {
+                perror("rudp_sendto(2)");
+                rudp_close(sock);
+                return -1;
+            }
+            fprintf(stdout, "data sent successfully!\n");
+            bytes_sent=0;
+        }
+    }
+    free(data);
+    fprintf(stdout, "Connection closed!\n");
+
+
+
+
+
+
+
+
     return 0;
 }
